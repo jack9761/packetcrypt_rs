d212ec2ccf40f86a8127816b219c237616d75bf2
jack9761 <86904468+jack9761@users.noreply.github.com>
2021-07-07 14:05:47 -0700

    Change build triplet pt2

--- contrib/github-actions/build-linux-binary.sh
+++ contrib/github-actions/build-linux-binary.sh
@@ -2,11 +2,11 @@
 
 function build() {
   cd "${GITHUB_WORKSPACE}" || exit
-  cargo build --release --features portable --target=armv7-unknown-linux-musl
+  cargo build --release --features portable --target=armv7-unknown-linux-gnueabihf
 
   mkdir "${GITHUB_WORKSPACE}/bin"
 
-  cp "${GITHUB_WORKSPACE}/target/armv7-unknown-linux-musl/release/packetcrypt" \
+  cp "${GITHUB_WORKSPACE}/target/armv7-unknown-linux-gnueabihf/release/packetcrypt" \
     "${GITHUB_WORKSPACE}"'/'"${RELEASE_NAME}"
 }
 build
