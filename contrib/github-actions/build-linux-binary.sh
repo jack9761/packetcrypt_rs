46601b3677e221e3904735173d80714d2b174483
jack9761 <86904468+jack9761@users.noreply.github.com>
2021-07-07 13:52:57 -0700

    More changes to get it to build to armv7

--- contrib/github-actions/build-linux-binary.sh
+++ contrib/github-actions/build-linux-binary.sh
@@ -2,11 +2,11 @@
 
 function build() {
   cd "${GITHUB_WORKSPACE}" || exit
-  cargo build --release --features portable --target=x86_64-unknown-linux-musl
+  cargo build --release --features portable --target=armv7-unknown-linux-musl
 
   mkdir "${GITHUB_WORKSPACE}/bin"
 
-  cp "${GITHUB_WORKSPACE}/target/x86_64-unknown-linux-musl/release/packetcrypt" \
+  cp "${GITHUB_WORKSPACE}/target/armv7-unknown-linux-musl/release/packetcrypt" \
     "${GITHUB_WORKSPACE}"'/'"${RELEASE_NAME}"
 }
 build
